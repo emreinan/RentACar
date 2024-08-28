ca9c1fcd6ba12f80eb1c53fb24cdd511f11c3e01
emreinan <emreinannn@gmail.com>
2024-08-28 14:04:54 +0300

    feat(Application) - CreateBrandCommand implemented feat(Persitence) - DbContext implemented

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,37 @@
+﻿using Application.Services.Repositories;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+{
+	public string Name { get; set; }
+
+	class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+	{
+		private readonly IBrandRepository _brandRepository;
+
+		public CreateBrandCommandHandler(IBrandRepository brandRepository)
+		{
+			_brandRepository = brandRepository;
+		}
+
+		public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+		{
+			var brand = new Brand(Guid.NewGuid(), request.Name);
+			await _brandRepository.AddAsync(brand);
+
+			return new CreatedBrandResponse
+			{
+				Id = brand.Id,
+				Name = brand.Name
+			};
+		}
+	}
+}
