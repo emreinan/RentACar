1e9eae3a59f7fff4106d6245273ab73908f6226d
emreinan <emreinannn@gmail.com>
2024-08-29 12:32:22 +0300

    Entities added

--- Domain/Entities/Brand.cs
+++ Domain/Entities/Brand.cs
@@ -10,11 +10,14 @@ namespace Domain.Entities;
 public class Brand : Entity<Guid>
 {
     public string Name { get; set; }
+
+    public virtual ICollection<Model> Models { get; set; }
     public Brand()
     {
-    }
-    public Brand(Guid id, string name)
-    {
+		Models = new HashSet<Model>();
+	}
+    public Brand(Guid id, string name) : this() // üstteki parametresiz constructor'ı da çalıştır diyoruz.
+	{
         Id = id;
         Name = name;
     }
