74e14d979616338287891f611e40c0f437352dce
emreinan <emreinannn@gmail.com>
2024-09-02 22:21:38 +0300

    MsSqlLogger implemented

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -37,7 +37,8 @@ public static class ApplicationServiceRegistration
 			configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
 		});
 
-		services.AddSingleton<LoggerServiceBase, FileLogger>();
+		//services.AddSingleton<LoggerServiceBase, FileLogger>();
+		services.AddSingleton<LoggerServiceBase, MsSqlLogger>();
 
 		return services;
 	}
